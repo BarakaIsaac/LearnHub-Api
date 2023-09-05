d32fd8acad515f3f7b63fe3c0b3cdb438096adeb
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-09-05 20:31:12 +0300

    Add status codes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,23 +5,23 @@ class CoursesController < ApplicationController
 
     def index
         courses = Course.all
-        render json: courses
+        render json: courses, status: :ok
     end
 
     def show
         course = Course.find(params[:id])
-        render json: course
+        render json: course, status: :ok
     end
 
     def update
         course = Course.find(params[:id])
         course.update!(course_params)
-        render json: course
+        render json: course, status: :ok
     end
 
     def create
         course = Course.create!(course_params)
-        render json: course
+        render json: course, status: :created
     end
 
     def destroy

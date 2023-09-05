5927cd3f7cc1280e68d657ec1ff946a086911a90
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-09-05 20:12:49 +0300

    create courses actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,33 @@
 class CoursesController < ApplicationController
+
+    def index
+        courses = Course.all
+        render json: courses
+    end
+
+    def show
+        course = Course.find(params[:id])
+        render json: course
+    end
+
+    def update
+        course = Course.find(params[:id])
+        courrse.update(course_params)
+        render json: course
+    end
+
+    def create
+        course = Course.create!(course_params)
+        render json: course
+    end
+
+    def destroy
+        course = Course.find(params[:id])
+        course.destroy
+        head :no_content
+    end
+
+    private
+
+    
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    validates :title, :description, :instructor, :creator_id, presence: true
+    
 end

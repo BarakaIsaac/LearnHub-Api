47c2b654eeb43c734db0bbb4761c579f166953ef
kepherogalo <kepher.ogalo@student.moringaschool.com>
2023-09-06 00:32:42 +0300

    creating enrolment routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # User routes
   post '/register', to: 'users#register'
   post '/login', to: 'users#login'
+  post '/enroll', to: 'users#enroll'
 
   resources :courses
+  resources :enrollments, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
